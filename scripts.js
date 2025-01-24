908bd95c4eda3b54812c71c171007043ad18d64a
VictorHenrique01 <vic10souz@gmail.com>
2025-01-24 17:40:46 -0300

    Cor alterada no form input e aprendendo função nova no scripts.js

--- scripts.js
+++ scripts.js
@@ -4,3 +4,21 @@ const inputUpload = document.getElementById("imagem-upload");
 uploadBtn.addEventListener("click", () => {
     inputUpload.click();
 })
+
+// Funcionalidade para leitura de arquivos abaixo:
+function lerConteudoDoArquivo(arquivo){
+    return new Promise((resolve, reject) => {
+        const leitor = new FileReader() //permite ler o conteúdo do arquivo;
+        leitor.onload = () => {
+            resolve({ url: leitor.result, nome: arquivo.name}) 
+        }
+
+        leitor.onerror = () => {
+            reject(`Erro na leitura do arquivo ${arquivo.name}`)
+        }
+
+        leitor.readAsDataURL(arquivo)
+    })
+}
+
+//Como lidar com assincronicidade da promessa:
